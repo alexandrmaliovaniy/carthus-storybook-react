b9ac7c68a1d7dcd01d30bde0d9a66a48740768b9
Alexandr Maliovaniy <alexandrmaliovaniy@gmail.com>
2023-03-11 19:37:16 +0200

    fix

--- story/story.schema.js
+++ story/story.schema.js
@@ -5,7 +5,6 @@ module.exports = (config) => {
         template: config.LoadTemplate(__dirname, "story.template.tsx"),
         inject: {
             import: [
-                "import { BaseInput } from \"@ui/index\";",
                 "import { ComponentMeta, ComponentStory } from \"@storybook/react\";",
                 "import { ComponentProps } from \"react\";"
             ]
